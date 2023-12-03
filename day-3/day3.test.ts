f6ebdf548f77c5ec8deeb9456d44bcea4392aeed
Patrick Reynolds <patrick.reynolds@imanage.com>
2023-12-03 18:24:38 +0000

    Day 3 start

---
+++ day-3/day3.test.ts
@@ -0,0 +1,18 @@
+import {stringToStringArray} from "../utils/helperMethods";
+
+const fs = require('fs');
+
+
+describe('Day two tests', () => {
+
+    let input = fs.readFileSync('day-3/input.txt', 'utf8');
+
+
+    it('', () => {
+
+
+        console.log()
+    });
+
+});
+
