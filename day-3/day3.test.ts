6ea3ba53dc20ec0755315cc502cc2004ae1bbace
Patrick Reynolds <patrick.reynolds@imanage.com>
2023-12-03 18:58:29 +0000

    Day 3 have logic for 3 chunk creation

--- day-3/day3.test.ts
+++ day-3/day3.test.ts
@@ -1,4 +1,5 @@
 import {stringToStringArray} from "../utils/helperMethods";
+import {partThreeCodeBreaker} from "./day3";
 
 const fs = require('fs');
 
@@ -8,8 +9,41 @@ describe('Day two tests', () => {
     let input = fs.readFileSync('day-3/input.txt', 'utf8');
 
 
-    it('', () => {
+    // check el in array before and after.
+    //
+    //     if either el has a symbol in the same indexes (-1 and +1) then add to record
 
+
+    it('adds all ', () => {
+
+        input = "*467..114..\n" +
+            "..........\n" +
+            "..35..#633.\n"
+
+        const schematicArray: string[] = stringToStringArray(input)
+
+
+        expect(partThreeCodeBreaker(schematicArray)).toBe(["*467..114..",
+        "..........",
+        "..35..#633."])
+
+    });
+
+    it('solves part 1!', () => {
+
+        input = "467..114..\n" +
+                "...*......\n" +
+                "..35..633.\n" +
+                "......#...\n" +
+                "617*......\n" +
+                ".....+.58.\n" +
+                "..592.....\n" +
+                "......755.\n" +
+                "...$.*....\n" +
+                ".664.598.."
+
+
+        expect(partThreeCodeBreaker(input)).toBe(4361)
 
         console.log()
     });
--- day-3/day3.ts
+++ day-3/day3.ts
@@ -2,4 +2,27 @@
 
 
 
+export const partThreeCodeBreaker = (schematicArray: string[]) => {
+
+
+    let numbersToAdd = []
+    const size = schematicArray.length
+
+    for (let i = 0; i < size; i++) {
+        const previous= i === 0 ? null : schematicArray[i - 1];
+        const current= schematicArray[i];
+        const next= i >= size ? null : schematicArray[i + 1];
+
+        const toCheck = [previous, current, next]
+
+        return toCheck
+
+        // do check and add to numbersToAdd
+    }
+
+
+}
+
+
+
 
